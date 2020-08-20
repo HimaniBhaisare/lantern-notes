3482fb7420eeaa71afdb3e49dbcf709a1a773308
aravindmathradan <aravindmathradan2@gmail.com>
2020-08-20 18:14:04 +0530

    firebase integration
    Co-authored-by: Himani <himanibhaisare1789@gmail.com>

--- server.js
+++ server.js
@@ -2,22 +2,13 @@ const express = require('express');
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-admin.initializeApp({
-    credential: admin.credential.cert(
-        JSON.parse(Buffer.from(process.env.GOOGLE_FIREBASE_AUTH, 'base64').toString('utf-8')))
-});
-
-const db = admin.firestore();
-
-data = {
-    name: "Kuku",
-    animal: "Panda"
-};
-
-const docref = db.collection('sampleDocs').doc('animals');
-docref.update(data);
-
 const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening at ${port}`));
 app.use(express.static('public'));
+
+admin.initializeApp({
+    credential: admin.credential.cert(
+        JSON.parse(Buffer.from(process.env.GOOGLE_FIREBASE_AUTH, 'base64').toString('utf-8')))
+});
+const db = admin.firestore();
