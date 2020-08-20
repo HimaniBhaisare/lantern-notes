522e815fc622bca65a9aa6d93e8d548858323a9e
aravindmathradan <aravindmathradan2@gmail.com>
2020-08-20 16:36:56 +0530

    firebase integration test#3

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ admin.initializeApp({
         "type": process.env.FIREBASE_TYPE,
         "project_id": process.env.FIREBASE_PROJECT_ID,
         "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-        "private_key" : process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        "client_email" : process.env.FIREBASE_CLIENT_EMAIL,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
         "client_id": process.env.FIREBASE_CLIENT_ID,
         "auth_uri": process.env.FIREBASE_AUTH_URI,
         "token_uri": process.env.FIREBASE_TOKEN_URI,
@@ -20,11 +20,13 @@ const db = admin.firestore();
 
 data = {
     name: "Himani",
-    animal: "Donkey"
+    animal: "Monkey"
 };
 
 db.collection('sampleDocs').doc('animals').update(data).then(() => {
     console.log('data added');
+}).catch(() => {
+    console.log("Error Occurred!");
 });
 
 const app = express();
