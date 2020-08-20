f99cf43be96a93171ad6e256a61e3010495fceba
aravindmathradan <aravindmathradan2@gmail.com>
2020-08-20 14:18:05 +0530

    firebase integration test#1

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
 const express = require('express');
+const admin = require('firebase-admin');
+require('dotenv').config();
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+        "type": process.env.FIREBASE_TYPE,
+        "project_id": process.env.FIREBASE_PROJECT_ID,
+        "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+        "private_key" : process.env.FIREBASE_PRIVATE_KEY,
+        "client_email" : process.env.FIREBASE_CLIENT_EMAIL,
+        "client_id": process.env.FIREBASE_CLIENT_ID,
+        "auth_uri": process.env.FIREBASE_AUTH_URI,
+        "token_uri": process.env.FIREBASE_TOKEN_URI,
+        "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+    })
+});
+const db = admin.firestore();
+
+data = {
+    name: "Himani",
+    animal: "Monkey"
+};
+
+db.collection('sampleDocs').doc('animals').update(data).then(() => {
+    console.log('data added');
+});
+
 const app = express();
 
 app.listen(5000, () => console.log("Listening in 5000"));
