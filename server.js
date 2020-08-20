065111f0f1a6d08b1f777c70e98b11956dfa1062
aravindmathradan <aravindmathradan2@gmail.com>
2020-08-20 17:54:20 +0530

    firebase integration test#5

--- server.js
+++ server.js
@@ -3,24 +3,15 @@ const admin = require('firebase-admin');
 require('dotenv').config();
 
 admin.initializeApp({
-    credential: admin.credential.cert({
-        "type": process.env.FIREBASE_TYPE,
-        "project_id": process.env.FIREBASE_PROJECT_ID,
-        "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-        "client_id": process.env.FIREBASE_CLIENT_ID,
-        "auth_uri": process.env.FIREBASE_AUTH_URI,
-        "token_uri": process.env.FIREBASE_TOKEN_URI,
-        "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-        "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
-    })
+    credential: admin.credential.cert(
+        JSON.parse(Buffer.from(process.env.GOOGLE_FIREBASE_AUTH, 'base64').toString('utf-8')))
 });
+
 const db = admin.firestore();
 
 data = {
     name: "Kuku",
-    animal: "Monkey"
+    animal: "Panda"
 };
 
 const docref = db.collection('sampleDocs').doc('animals');
