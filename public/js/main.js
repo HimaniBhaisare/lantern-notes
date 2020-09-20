3a4036356de9f34c4f3897067e36094e58390059
aravindmathradan <aravindmathradan2@gmail.com>
2020-09-20 17:24:11 +0530

    minor bug fixes for collab alpha version

--- public/js/main.js
+++ public/js/main.js
@@ -23,6 +23,14 @@ const signupError = document.getElementById("signupError");
 const fpError = document.getElementById("fpError");
 const sessionIdspan = document.getElementById("sessionIdSpan");
 
+
+window.addEventListener('beforeunload', function (e) {
+    // Cancel the event
+    e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+    // Chrome requires returnValue to be set
+    e.returnValue = '';
+});
+
 // Hide loading screen after 2 secs
 function loadingFade() {
     setTimeout(() => {
